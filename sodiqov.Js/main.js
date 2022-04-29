22cf650ffdaf5a4cb20ffd779b7aa44207a4b388
Shoxbozbek <99474362+Shoxbozbek@users.noreply.github.com>
2022-04-29 14:27:06 +0500

    Add files via upload

---
+++ sodiqov.Js/main.js
@@ -0,0 +1,76 @@
+let player = document.querySelector('.player')
+let gate = document.querySelector('.gate')
+let ball = document.querySelector('.ball')
+let btn1 = document.querySelector('.btn1')
+let btn2 = document.querySelector('.btn2')
+let btn3 = document.querySelector('.btn3')
+let blow = Math.floor(Math.random()*30)
+
+btn1.addEventListener('click',function(){
+    blow = Math.floor(Math.random()*30)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    color random cpy
+
+
+// let color_text  = document.querySelector('#color_text')
+// let body = document.querySelector('.body')
+// let btn = document.querySelector('button')
+
+// btn.addEventListener('click',function () {
+//     const rand = Math.random()*60
+//     console.log(rand);
+//     if (rand < 10) {
+//         color_text.textContent = "blue"
+//         body.style.backgroundColor = 'blue'
+//     }
+//     else if(rand < 20){
+//         color_text.textContent = "aqua"
+//         body.style.backgroundColor = "aqua"
+//     }
+//     else if(rand < 30){
+//         color_text.textContent = "green"
+//         body.style.backgroundColor = "green"
+//     }
+//     else if(rand < 40){
+//         color_text.textContent = "red"
+//         body.style.backgroundColor = "red"
+//     }
+//     else if(rand < 50){
+//         color_text.textContent = "yellow"
+//         body.style.backgroundColor = "yellow"
+//     }
+//     else if(rand < 60){
+//         color_text.textContent = "tomato"
+//         body.style.backgroundColor = "tomato"
+//     }
+    
+// })
+
